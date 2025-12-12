8c9cac6f707d6ef418f0df909acf11dd06c14c96
Colin <lanoizeletcolin@gmail.com>
2025-12-12 23:30:39 +0100

    modified:   BipBoup.class 	modified:   BipBoup.java	new file:   Joueur.class  modified:   Joueur.java 	modified:   BipBoup.class 	modified:   BipBoup.java

--- BipBoup.java
+++ BipBoup.java
@@ -6,9 +6,20 @@ remplacer(string s, string avant, string apres)
 
 class BipBoup extends Program{
 
-    /*final Joueur McCree = new Joueur();
-    final Joueur Cassidy = new Joueur();
-    */
+// Création des joueurs
+    final Joueur McCree = newJoueur("McCree", 100, 100, new boolean[]{true, true});
+    final Joueur Cassidy = newJoueur("Cassidy", 100, 100, new boolean[]{true, true});
+
+    Joueur newJoueur(String nom, int HP_max, int HP, boolean[] soin){
+        Joueur j = new Joueur();
+        j.nom = nom;
+        j.HP_max = HP_max;
+        j.HP = HP;
+        j.soin = soin;
+        return j;
+    }
+
+// endgroupe
 
     int nbLignes(String nomFichier){
         int lines = 0;
@@ -26,25 +37,12 @@ class BipBoup extends Program{
             println(readLine(fichier));
         }
     }
-        
     
-
-   /* boolean partieFinie(){
+    boolean partieFinie(){
         return (Cassidy.HP < 0 || McCree.HP < 0);
     }
-   
-    
-   Joueur newJoueur(int HP_max, int HP, boolean[] soin){
-        Joueur j = new Joueur();
-        j.HP_max = HP_max;
-        j.HP = HP;
-        j.soin = soin;
-        return j;
-    }
-     */
+     
     boolean ChoixDuPremierJoueur(){
-        
-
         println("McCree, appuie sur entrée pour lancer le dé.");
         readString();
         int lancer1 = random(1,6);
@@ -54,8 +52,10 @@ class BipBoup extends Program{
         int lancer2 = random(1,6);
         println("Vous avez obtenu un " + lancer2 + " !");
         if (lancer1 > lancer2){
+            println("C'est donc à McCree de commencer");
             return true;
         } else if (lancer2 > lancer1){
+            println("C'est donc à Cassidy de commencer");
             return false;
         } else {
             println("Egalité ! Vous avez tous les deux obtenus " + lancer2 + ", vous devez relancer le dé.");
@@ -64,25 +64,25 @@ class BipBoup extends Program{
     }
 
     void algorithm(){
-        char choice;
+        String choice;
         do {
-            niquez vos McCree
-            dump("menu.txt");
-            choice = readChar();
-            if (choice == '1'){
+            dump("menu.txt");2
+            
+            choice = readString();
+            if (equals(choice,"1")){
                 dump("regles.txt");
                 readString();
-            } else if (choice == '2'){
-                println("Tout d'abord, le joueur 1 incarnera McCree et le joueur 2 incarnera Cassidy.");
-                println("Maintenant, on va lancer des dés pour déterminer qui commencera.");
-                boolean McCreeTurn = ChoixDuPremierJoueur();   
-            } else if (choice == '3'){
+            }else if (equals(choice,"2")){
+            }else if (equals(choice,"3")){
                 System.exit(0);
-            } else {
-                println("choisissez une nombre entre 1 et 3");
-                readChar();
-            }
-        } while (choice != '2');
-        
+            }else{
+            println("Veuillez choisir un chiffre entre 1 et 3");
+            sleep(1000);
+                }
+        } while (!equals(choice,"2"));
+
+            println("Tout d'abord, le joueur 1 incarnera McCree et le joueur 2 incarnera Cassidy.");
+            println("Maintenant, on va lancer des dés pour déterminer qui commencera.");
+            boolean McCreeTurn = ChoixDuPremierJoueur();  
     }
 }
